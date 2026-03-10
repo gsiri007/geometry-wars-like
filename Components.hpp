3a49df7d953f13501cc1ae373017746d3524e08a
gsiri007 <216904534+gsiri007@users.noreply.github.com>
2026-03-11 05:10:50 +0530

    Component classes

---
+++ Components.hpp
@@ -0,0 +1,80 @@
+#include "Vec2.hpp"
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <cstddef>
+
+
+class CTransform
+{
+public:
+  Vec2  position  { 0.0f, 0.0f };
+  Vec2  velocity  { 0.0f, 0.0f };
+  float angle     { 0.0f };
+
+  CTransform(const Vec2 & p, const Vec2 & v, float a)
+    : position(p)
+    , velocity(v)
+    , angle(a)
+  {};
+};
+
+class CShape
+{
+public:
+  sf::CircleShape circle;
+
+  CShape( float radius
+        , size_t vertices
+        , const sf::Color & fillColor
+        , const sf::Color & outlineColor
+        , float outlineThickness)
+    : circle(radius, vertices)
+  {
+    circle.setFillColor(fillColor);
+    circle.setOutlineColor(outlineColor);
+    circle.setOutlineThickness(outlineThickness);
+    circle.setOrigin(sf::Vector2f(radius, radius));
+  }
+};
+
+class CCollision
+{
+public:
+  float radius { 0.0f };
+
+  CCollision(float r) : radius(r) {};
+};
+
+class CScore
+{
+public:
+  size_t score { 0 };
+
+  CScore(size_t s) : score(s) {};
+};
+
+class CLifespan
+{
+public:
+  // FTL - Frames To Live
+  size_t initialFrames   { 0 };  // initial FTL
+  size_t remainingFrames { 0 };  // remaining FTL
+
+  CLifespan(size_t framesToLive)
+    : initialFrames(framesToLive)
+    , remainingFrames(framesToLive)
+  {}
+};
+
+class CInput
+{
+public:
+  bool  up     { false };
+  bool  down   { false };
+  bool  left   { false };
+  bool  right  { false };
+  bool  shoot  { false };
+
+  CInput() {};
+};
